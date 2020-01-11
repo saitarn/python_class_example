9c2c3dfa5242d3ba6b0a6d44efd0bef5fdefbf35
admin <saitarn14@gmail.com>
2020-01-11 23:50:08 +0700

    init commit

---
+++ example.py
@@ -0,0 +1,21 @@
+class NameOfClass():
+    class_attribute = 'value'
+
+    # constructor
+    def __init__(self, param1, param2):
+        self.param1 = param1
+        self.param2 = param2
+
+    def method(self):
+        # CODE HERE
+        pass
+
+    @classmethod
+    def cls_method(cls, param1, param2):
+        # CODE HERE
+        pass
+
+    @staticmethod
+    def stc_method(param2, param2):
+        # CODE HERE
+        pass
